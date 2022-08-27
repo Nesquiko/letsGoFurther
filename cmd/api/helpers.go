24277d12d4c1fd6ff30f18b38c8e262c2fb7a508
Nesquiko <lukas.castven@gmail.com>
2022-08-27 18:17:51 +0200

    chapter 4

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -31,6 +34,67 @@ func (app *application) writeJSON(
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(json)
+
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		var syntaxErr *json.SyntaxError
+		var unmarshalTypeErr *json.UnmarshalTypeError
+		var invalidUnmarshalErr *json.InvalidUnmarshalError
+
+		invalidFieldPrefix := "json: unknown field "
+		largeBodyErrorStr := "http: request body too large"
+
+		switch {
+		case errors.As(err, &syntaxErr):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxErr.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeErr):
+			if unmarshalTypeErr.Field != "" {
+				return fmt.Errorf(
+					"body contains incorrect JSON type for field %q",
+					unmarshalTypeErr.Field,
+				)
+			}
+			return fmt.Errorf(
+				"body contains incorrect JSON type (at character %d)",
+				unmarshalTypeErr.Offset,
+			)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case strings.HasPrefix(err.Error(), invalidFieldPrefix):
+			fieldName := strings.TrimPrefix(err.Error(), invalidFieldPrefix)
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		case err.Error() == largeBodyErrorStr:
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
+		case errors.As(err, &invalidUnmarshalErr):
+			panic(err)
+
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
 
 	return nil
 }
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,15 +1,43 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+const minsSuffix = "mins"
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 func (r Runtime) MarshallJSON() ([]byte, error) {
 	value := fmt.Sprintf("%d mins", r)
 
 	quoted := strconv.Quote(value)
 	return []byte(quoted), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquoted, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquoted, " ")
+
+	if len(parts) != 2 || parts[1] != minsSuffix {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
